bc3edb22dce2f4e7499d8e1f54ddbd25f89516c6
abdoulaye <abdelraul140385@gmail.com>
2022-12-24 20:14:15 +0000

    modification de exo2

---
+++ exo2/ex02-a/js/script.js
@@ -0,0 +1,14 @@
+
+// exercice2-a :
+         let X = parseInt(prompt("Entrez un nombre entier SVP"));
+          for(let i=0 ; i<=X ;i++){
+
+               if(i%2==0){
+                  console.log(i)  
+               }
+               
+          }
+
+         
+
+
---
+++ exo2/ex02-b/js/script.js
@@ -0,0 +1,13 @@
+
+// exercice2-b :
+         
+
+         let Y = parseInt(prompt("Entrez un nombre entier SVP"));
+          for(let i=0 ; i<=Y ;i++){
+               if(i%2==1){
+                   console.log(i) 
+               }
+               
+          }
+
+
